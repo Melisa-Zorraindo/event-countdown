536934e0cb9b58bc7980e5fef7d154f434b5c34a
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-07-24 17:27:07 +0200

    create colour selectors event heading

--- js/modalPopups/headingModal.js
+++ js/modalPopups/headingModal.js
@@ -1,7 +1,31 @@
-const eventHeadingBg = document.querySelector("#colour-bg-event-name-picker");
+const eventHeadingBg = document.querySelector("#bg-colour-event-name-picker");
+const eventHeadingFontColour = document.querySelector(
+  "#font-colour-event-name-picker"
+);
 
 export function displayHeadingEditionOptions() {
   //create event bg colour picker
   eventHeadingBg.innerHTML = "";
-  console.log("test");
+  createColourSelector(eventHeadingBg, "Background colour");
+
+  //create font colour picker
+  eventHeadingFontColour.innerHTML = "";
+  createColourSelector(eventHeadingFontColour, "Font colour");
+}
+
+function createColourSelector(container, labelName) {
+  //create label
+  const label = document.createElement("label");
+  label.innerHTML = labelName;
+  container.append(label);
+  //create colour selector
+  const colourPicker = document.createElement("input");
+  colourPicker.type = "color";
+  colourPicker.id = "colour-picker";
+  colourPicker.value = " ";
+  container.append(colourPicker);
+  //add event listener
+  colourPicker.addEventListener("change", () => {
+    console.log("test");
+  });
 }
