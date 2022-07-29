0f4ee269131cb56a0603964278b8fc4b78359d97
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-07-29 12:28:22 +0200

    render next month calendar

--- js/modalPopups/calendar/calendar.js
+++ js/modalPopups/calendar/calendar.js
@@ -2,7 +2,7 @@ import { year } from "./calendarComponents.js";
 import { week } from "./calendarComponents.js";
 
 const modalCalendar = document.querySelector("#modal-calendar");
-const calendarContainer = document.querySelector("#calendar");
+const calendarCard = document.querySelector("#calendar");
 const date = new Date();
 const currentDay = date.getDate();
 const currentMonth = date.getMonth();
@@ -11,48 +11,90 @@ const currentYear = date.getFullYear();
 export function displayCalendar() {
   modalCalendar.classList.remove("hidden");
 
-  calendarContainer.innerHTML = "";
-  const calendarBox = document.createElement("div");
-  calendarBox.classList.add("modal-box");
-  calendarContainer.append(calendarBox);
+  calendarCard.innerHTML = "";
+  const calendarContainer = document.createElement("div");
+  calendarContainer.classList.add("modal-box");
+  calendarCard.append(calendarContainer);
 
   const monthYearBox = document.createElement("div");
   monthYearBox.classList.add("calendar-heading");
-  calendarBox.append(monthYearBox);
+  calendarContainer.append(monthYearBox);
 
-  const previousYear = document.createElement("i");
-  previousYear.classList.add("fas", "fa-caret-square-left");
-  previousYear.id = "previuos-year";
-  monthYearBox.append(previousYear);
+  const previous = document.createElement("i");
+  previous.classList.add("fas", "fa-caret-square-left");
+  previous.id = "previuos";
+  monthYearBox.append(previous);
 
   const monthYearHeading = document.createElement("h3");
   monthYearHeading.innerHTML = `${year[currentMonth].month} ${currentYear}`;
   monthYearBox.append(monthYearHeading);
 
-  const nextYear = document.createElement("i");
-  nextYear.classList.add("fas", "fa-caret-square-right");
-  nextYear.id = "next-year";
-  monthYearBox.append(nextYear);
+  const next = document.createElement("i");
+  next.classList.add("fas", "fa-caret-square-right");
+  next.id = "next";
+  monthYearBox.append(next);
 
-  const daysBox = document.createElement("div");
-  daysBox.classList.add("calendar-box");
-  calendarBox.append(daysBox);
+  const calendarBox = document.createElement("div");
+  calendarBox.classList.add("calendar-box");
+  calendarContainer.append(calendarBox);
+
+  const weekdaysBox = document.createElement("div");
+  weekdaysBox.classList.add("calendar-weekday");
+  calendarBox.append(weekdaysBox);
 
   week.forEach((day) => {
     const weekDay = document.createElement("div");
     weekDay.innerHTML += day;
     weekDay.classList.add("weekday");
-    daysBox.append(weekDay);
+    weekdaysBox.append(weekDay);
   });
+
+  const dayBox = document.createElement("div");
+  dayBox.classList.add("day-box");
+  calendarBox.append(dayBox);
 
   //start day of week on right day
   const firstDay = new Date(`${year[currentMonth].month} 1 ${currentYear}`);
   const dayOffset = firstDay.getDay();
 
+  renderCalendarDays(dayOffset, dayBox);
+
+  //update calendar to next month
+  next.addEventListener("click", () => {
+    displayFollowingMonth(monthYearHeading, dayBox);
+  });
+}
+
+let monthAddition = 0;
+let yearAddition = 0;
+let monthRestart = year[0].month;
+function displayFollowingMonth(heading, calendarContainer) {
+  //display following month
+  monthAddition++;
+  let followingMonth = year[currentMonth + monthAddition].month;
+  heading.innerHTML = `${followingMonth} ${currentYear}`;
+
+  //display calendar for matching month
+  calendarContainer.innerHTML = "";
+
+  //start day of week on right day
+  const startDay = new Date(
+    `${year[currentMonth + monthAddition].month} 1 ${currentYear}`
+  );
+  const dayOffset = startDay.getDay();
+  renderCalendarDays(dayOffset, calendarContainer);
+
+  //display following year if applicable
+
+  //deactivate button if applicable
+}
+
+function renderCalendarDays(dayOffset, container) {
+  //start day of week on right day
   for (let i = 1; i < dayOffset; i++) {
     const emptyDay = document.createElement("div");
     emptyDay.classList.add("calendar-day", "inactive");
-    daysBox.append(emptyDay);
+    container.append(emptyDay);
   }
 
   const dayNumber = year[currentMonth].days;
@@ -63,8 +105,6 @@ export function displayCalendar() {
     if (i < currentDay) {
       day.classList.add("inactive");
     }
-    daysBox.append(day);
+    container.append(day);
   }
-
-  //update calendar to next month
 }
