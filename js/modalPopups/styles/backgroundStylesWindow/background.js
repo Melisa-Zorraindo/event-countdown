0d66f129156828dc8ad040090babec76de1df996
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-08-10 06:45:22 +0200

    clear choices button (background) functionality

--- js/modalPopups/styles/backgroundStylesWindow/background.js
+++ js/modalPopups/styles/backgroundStylesWindow/background.js
@@ -6,9 +6,24 @@ const OVERLAY = document.querySelector(".overlay");
 const BACKGROUND_SAVE_BUTTON = document.querySelector(
   "#save-background-preferances-button"
 );
+const BACKGROUND_REMOVE_BUTTON = document.querySelector(
+  "#remove-background-preferances-button"
+);
 
 BACKGROUND_SAVE_BUTTON.addEventListener("click", () => {
   //hide modal window
   BACKGROUND_EDITION_WINDOW.classList.add("hidden");
   OVERLAY.classList.add("hidden");
+});
+
+BACKGROUND_REMOVE_BUTTON.addEventListener("click", () => {
+  //remove background image
+  let bgImg = localStorage.getItem("backgroundImage");
+  if (bgImg) {
+    localStorage.removeItem("backgroundImage");
+  }
+  //hide modal window
+  BACKGROUND_EDITION_WINDOW.classList.add("hidden");
+  OVERLAY.classList.add("hidden");
+  location.reload();
 });
