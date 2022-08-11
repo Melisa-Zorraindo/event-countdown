e2149ccd58b8bde394727f18515cceb20da71af5
Melisa <76968122+Melisa-Zorraindo@users.noreply.github.com>
2022-08-11 12:15:39 +0200

    Update selectPicture.js

--- js/modalPopups/styles/backgroundStylesWindow/selectPicture.js
+++ js/modalPopups/styles/backgroundStylesWindow/selectPicture.js
@@ -1,5 +1,4 @@
 export function setBackgroundImage(image, localStorageKey) {
   document.body.style.background = `url("${image}") center/cover no-repeat`;
   localStorage.setItem(localStorageKey, image);
-  localStorage.removeItem("screenColour");
 }
