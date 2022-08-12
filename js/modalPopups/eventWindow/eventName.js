736ecb0735e122a2a382bb4d10e6d0ace55c2f15
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-08-12 11:29:20 +0200

    Update eventName.js

--- js/modalPopups/eventWindow/eventName.js
+++ js/modalPopups/eventWindow/eventName.js
@@ -2,7 +2,7 @@ const eventTitle = document.querySelector("h1");
 const newHeading = document.querySelector("#event-name");
 
 export function updateEventTitle() {
-  let title = newHeading.value;
+  let title = newHeading.value || "Event title";
   eventTitle.innerHTML = "";
   eventTitle.innerHTML += title;
   localStorage.setItem("eventTitle", title);
